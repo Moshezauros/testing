1e2d276b08419808b3c19a889c0f4c4e5a7ef638
moshet <moshe@cloudshare.com>
2019-05-27 14:29:57 +0300

    wip

--- simple.sh
+++ simple.sh
@@ -1,6 +1,16 @@
 #! /bin/bash
+git clone https://github.com/Moshezauros/forgeops.git
 
-# install gcloud
+# edit variables
+# project and service account
+PROJECT="$(gcloud config get-value project)"
+SA="$(gcloud config get-value account)"
+# J=$(printf "sed -i 's/{PROJECT_ID_TO_REPLACE}/%s/g' ./forgeops/etc/gke-env.cfg" $PROJECT)
+# echo "${J}"
 
-# clone git repository
-git clone https://github.com/ForgeRock/forgeops.git
+sed -i "s/{PROJECT_ID_TO_REPLACE}/%PROJECT/g" ./forgeops/etc/gke-env.cfg
+sed -i "s/{SERVICE_ACCOUNT_TO_REPLACE}/%SA/g" ./forgeops/etc/gke-env.cfg
+
+# ?ingress ip
+
+# run bg-bke-up.sh
