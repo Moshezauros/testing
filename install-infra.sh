ad8eebbbda806582c2ba594b89631dd7833a5c79
moshet <moshe@cloudshare.com>
2019-05-27 15:15:34 +0300

    still wip

---
+++ install-infra.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+run kubectl --help to validate its installed
+
+wget  -P ./ "https://storage.googleapis.com/kubernetes-helm/helm-v2.12.3-linux-amd64.tar.gz"
+tar -zxvf helm-v2.12.3-linux-amd64.tar.gz
+mv ./helm /bin/helm
+
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
--- simple.sh
+++ simple.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+./testing/install-infra.sh
+
 git clone https://github.com/Moshezauros/forgeops.git
 
 # edit variables
@@ -8,9 +10,12 @@ SA="$(gcloud config get-value account)"
 # J=$(printf "sed -i 's/{PROJECT_ID_TO_REPLACE}/%s/g' ./forgeops/etc/gke-env.cfg" $PROJECT)
 # echo "${J}"
 
-sed -i "s/{PROJECT_ID_TO_REPLACE}/%PROJECT/g" ./forgeops/etc/gke-env.cfg
-sed -i "s/{SERVICE_ACCOUNT_TO_REPLACE}/%SA/g" ./forgeops/etc/gke-env.cfg
+sed -i "s/{PROJECT_ID_TO_REPLACE}/$PROJECT/g" ./forgeops/etc/gke-env.cfg
+sed -i "s/{SERVICE_ACCOUNT_TO_REPLACE}/$SA/g" ./forgeops/etc/gke-env.cfg
 
 # ?ingress ip
 
 # run bg-bke-up.sh
+cd ./forgeops/bin
+bash ./bg-gke-up.sh
+bash ./bg-deploy-prometheus.sh
