209fe2951db465c89dbb7cdee4a229aaa669691d
moshet <moshe@cloudshare.com>
2019-05-28 13:42:43 +0300

    wip

--- simple.sh
+++ simple.sh
@@ -15,10 +15,15 @@ sed -i "s/{SERVICE_ACCOUNT_TO_REPLACE}/$SA/g" ./forgeops/etc/gke-env.cfg
 echo $(gcloud config list)
 echo $(kubectl version)
 
+# long shot from the web
+sudo cp /etc/kubernetes/admin.conf $HOME/
+sudo chown $(id -u):$(id -g) $HOME/admin.conf
+export KUBECONFIG=$HOME/admin.conf
+
 # ?ingress ip
 
 # run bg-bke-up.sh
-chmod -R 777 ./forgeops
-cd ./forgeops/bin
-bash ./bg-gke-up.sh
-bash ./bg-deploy-prometheus.sh
+# chmod -R 777 ./forgeops
+# cd ./forgeops/bin
+# bash ./bg-gke-up.sh
+# bash ./bg-deploy-prometheus.sh
