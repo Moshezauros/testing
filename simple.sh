a41cebf2cbdedcdb4c710eadf9fb8906fdafd23a
moshet <moshe@cloudshare.com>
2019-05-28 14:06:06 +0300

    wip

--- simple.sh
+++ simple.sh
@@ -12,19 +12,12 @@ sed -i "s/{PROJECT_ID_TO_REPLACE}/$PROJECT/g" ./forgeops/etc/gke-env.cfg
 sed -i "s/{SERVICE_ACCOUNT_TO_REPLACE}/$SA/g" ./forgeops/etc/gke-env.cfg
 
 # TO-DO: remove
-echo $(gcloud config list)
-echo $(kubectl version)
+gcloud config set container/use_client_certificate True
 
-# long shot from the web
-sudo cp /etc/kubernetes/admin.conf $HOME/
-sudo chown $(id -u):$(id -g) $HOME/admin.conf
-export KUBECONFIG=$HOME/admin.conf
-
-echo $(kubectl version)
 # ?ingress ip
 
 # run bg-bke-up.sh
-# chmod -R 777 ./forgeops
-# cd ./forgeops/bin
-# bash ./bg-gke-up.sh
-# bash ./bg-deploy-prometheus.sh
+chmod -R 777 ./forgeops
+cd ./forgeops/bin
+bash ./bg-gke-up.sh
+bash ./bg-deploy-prometheus.sh
