c98e7c0645c1f2f4016f2cf04ea3ec3a9d6ddaee
moshet <moshe@cloudshare.com>
2019-05-28 09:13:13 +0300

    improvement is depcrecated

--- install-infra.sh
+++ install-infra.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
 # kubectl
-# curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-# chmod +x ./kubectl
-# sudo mv ./kubectl /usr/local/bin/kubectl
-# run kubectl --help to validate its installed
-gcloud components install kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
 
 # helm version 2.12.3
 wget  -P ./ "https://storage.googleapis.com/kubernetes-helm/helm-v2.12.3-linux-amd64.tar.gz"
