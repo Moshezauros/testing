096ff5b65c2a76b1f956bc354689dec9254f4389
moshet <moshe@cloudshare.com>
2019-05-30 08:43:03 +0300

    a

--- install-infra.sh
+++ install-infra.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 # kubectl
-whoami
-echo "$($PWD)"
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mv ./kubectl /usr/local/bin/kubectl
+
+# create kubeconfig dir and file
+mkdir $HOME/.kube
+echo "" > $HOME/.kube/config
 
 # helm version 2.12.3
 wget  -P ./ "https://storage.googleapis.com/kubernetes-helm/helm-v2.12.3-linux-amd64.tar.gz"
