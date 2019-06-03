e1cda8720865996dad6003d2dd285961ef6f56e8
moshet <moshe@cloudshare.com>
2019-06-03 09:11:57 +0300

    aa

--- simple.sh
+++ simple.sh
@@ -6,8 +6,6 @@ git clone -b release/6.5.1 https://github.com/ForgeRock/forgeops.git
 
 # copy relevant files from here to forgeops folder
 cp -p ./testing/bg-files/bg-deploy-prometheus.sh ./testing/bg-files/bg-gke-create-cluster.sh ./testing/bg-files/bg-gke-up.sh /forgeops/bin/
-# cp ./testing/bg-files/bg-gke-create-cluster.sh /forgeops/bin/bg-gke-create-cluster.sh
-# cp ./testing/bg-files/bg-gke-up.sh /forgeops/bin/bg-gke-up.sh
 cp -p ./testing/bg-files/gke-env.cfg /forgeops/etc/
 cp -p ./testing/bg-files/bg-kube-prometheus.yaml /forgeops/etc/prometheus-values/
 
@@ -19,7 +17,7 @@ SA="$(gcloud config get-value account)"
 sed -i "s/{PROJECT_ID_TO_REPLACE}/$PROJECT/g" ./forgeops/etc/gke-env.cfg
 sed -i "s/{SERVICE_ACCOUNT_TO_REPLACE}/$SA/g" ./forgeops/etc/gke-env.cfg
 
-# TO-DO: remove
+# this is done to use the service account credentials (in this case, the key in creds.json)
 # see: https://github.com/kubernetes/kubernetes/issues/30617
 gcloud config set container/use_client_certificate False
 
@@ -29,8 +27,10 @@ touch /.kube/config
 chmod -R 777 /.kube
 export KUBECONFIG=/.kube/config
 
+# consider moving this to terraform script
 # ingress ip - create static ip and save to env variables
-gcloud compute addresses create ip-1 --global
+# gcloud compute addresses create ip-1 --global
+gcloud compute addresses create ip-1 --region us-central1
 ip=$(gcloud compute addresses list | grep 'ip-1' -m1 | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')
 echo $ip
 sed -i "s/{IP_TO_REPLACE}/$ip/g" ./forgeops/etc/gke-env.cfg
@@ -40,7 +40,7 @@ chmod -R 777 ./forgeops
 cd ./forgeops/bin
 bash ./bg-gke-up.sh
 
-# uncomment to enable monitoring
+# uncomment to enable monitoring, or update bg-gke-up.sh 
 # bash ./bg-deploy-prometheus.sh
 
 # this is the student part
